abbc9f5416fc7bacc2f021b28b2e753da4208f87
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2023-12-19 19:51:29 -0400

    Create Form
    create form with inputs and table

---
+++ HospitalManagementApplication/src/main/java/com/mycompany/hospitalmanagementapplication/HospitalManagementApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospitalmanagementapplication;
+
+/**
+ *
+ * @author pesanth
+ */
+public class HospitalManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
